f8ff9b65bcf01d43cdf18998cde524cbb3795f40
“jun161995” <bxlove88@gmail.com>
2021-11-16 20:51:54 +0900

    例外処理を追加

--- app/src/main/java/jp/techacademy/junichirou/yoshihara/calcapp/MainActivity.kt
+++ app/src/main/java/jp/techacademy/junichirou/yoshihara/calcapp/MainActivity.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 import android.content.Intent
-import kotlinx.android.synthetic.main.activity_main2.*
+import android.util.Log
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
     var value1: Float = 0.0f
@@ -26,23 +26,29 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onClick(v: View) {
         val intent = Intent(this, MainActivity2::class.java)
-        value1 = editText1.text.toString().toFloat()
-        value2 = editText2.text.toString().toFloat()
-        when (v.id) {
-            R.id.button1 -> {
-                value3 = value1 + value2
-            }
-            R.id.button2 -> {
-                value3 = value1 - value2
-            }
-            R.id.button3 -> {
-                value3 = value1 * value2
-            }
-            R.id.button4 -> {
-                value3 = value1 / value2
-            }
+        try {
+            value1 = editText1.text.toString().toFloat()
+            value2 = editText2.text.toString().toFloat()
+        } catch (e : Exception) {
+            Log.d("CalcApp", "値が不正です")
+        } finally {
         }
-        intent.putExtra("value3", value3)
+            when (v.id) {
+                R.id.button1 -> {
+                    value3 = value1 + value2
+                }
+                R.id.button2 -> {
+                    value3 = value1 - value2
+                }
+                R.id.button3 -> {
+                    value3 = value1 * value2
+                }
+                R.id.button4 -> {
+                    value3 = value1 / value2
+                }
+            }
+
+            intent.putExtra("value3", value3)
         startActivity(intent)
     }
 }
